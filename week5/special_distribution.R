f0b1f515b7d97c03a55fa153e000c51bbdcac83a
Nguyet Anh Nguyen <nna0702@gmail.com>
2017-03-09 23:19:25 +0000

    Add more distribution

--- week5/special_distribution.R
+++ week5/special_distribution.R
@@ -23,7 +23,7 @@ u <- runif(10000,0,1)
       plot(density_y, type = "l", xlim = c(0,2), main = "pdf of inverse exp", col = "blue", lwd = 3, xlab = "")
       
   
-  ##Approach 2: Plot the inverse of cdf of the exponential using qexp
+  ##Approach 2: Plot the inverse of cdf of the exponential using qexp - inverse of cdf (pexp)
       
       pdf("inverse_exponential.pdf")
       y_qexp <- qexp(u, rate = 3)
@@ -33,12 +33,46 @@ u <- runif(10000,0,1)
       
       plot(density_y_qexp, type = "l", xlim = c(0,2), main = "pdf of inverse exponential function", col = "yellow", lwd = 3, xlab = "")
       
-  ##Approach 3: Compare to random draws straight from the exponential distribution
+  ##Approach 3: Compare to random draws straight from the exponential distribution. Function rexp - random generation from exponential distribution
       pdf("random_draw.pdf")
       y_rexp <- rexp(10000, rate = 3)
       density_y_rexp <- density(y_rexp)
       hide <- dev.off()
       plot(density_y_rexp, type = "l", xlim = c(0,2), main = "random draw from exp distribution", lwd = 3, col = "red", xlab = "")
       
-      #*** NOte: Need to clarify the dev.off(). Without this, plots do not show up???
+  #*** NOte: Need to clarify the dev.off(). Without this, plots do not show up???
+  #*** NOte: Approach 1 and 2 use inverse sampling method
+      
+#Excercise 2: Consider a deck of 52 cards. Suppose that you sample 10 cards from the deck without replacing the cards. What is the probability that exactly five of the cards are hearts?
+    #Compute density of a hypergeometric distribution
+      dhyper(5,13,39,10)
+      
+#Excercise 3: Functions associated with ormal distribution
+    #Generate 1000 number from a normal with mean = 3, sd = 0.25
+      v <- rnorm(1000,3,0.25)
+    
+    #Probability density function - find the hieght of the normal with mean = 0 and sd = 0.5
+      dnorm(0,0,0.5)
+      
+    #Cumulative Distribution Function - Area under the standard normal curve to the left of 1.96. Lower.tail = FALSE is default - what is to the right
+      pnorm(1.96,0,1)
+      
+    #Quantile function is the inverse of pnorm. Find the value at which cdf of standard normal is 0.975
+      qnorm(0.975,0,1)
+      
+#Excercise 4: Plot pdf of normal distribution
+      
+  ##Approach 1: Inverse sampling method
+      y_qnorm <- qnorm(u)
+      density_y_qnorm <- density(y_qnorm, bw = 1)
+      pdf("normal.pdf")
+      hide <- dev.off()
+      plot(density_y_qnorm, type = "l", xlim = c(-5,5), main = "pdf of normal distribution", col = "navyblue", lwd = 3, xlab = "")
+  
+  ##Approach 2: Random generation from normal distribution
+      y_rnorm <- rnorm(10000,0,1)
+      density_y_rnorm <- density(y_rnorm, bw = 1)
+      pdf("random_norma.pdf")
+      hide <- dev.off()
+      plot(density_y_rnorm, type = "l", xlim = c(-5,5), main = "pdf of normal distribution from random draw", col = rgb(0.4,0.5,0.8,0.7), lwd = 3, xlab = "")
       
