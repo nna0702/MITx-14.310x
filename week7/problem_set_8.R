cfb5d201177102796554c59bee5376c1656c1c08
Nguyet Anh Nguyen <nna0702@gmail.com>
2017-03-24 00:58:13 +0000

    update week 7

--- week7/problem_set_8.R
+++ week7/problem_set_8.R
@@ -12,19 +12,24 @@ data_ps8 <- read.csv("C://Users//nguyetanh//Documents//MITx-14.310x//week7//teac
 #-------------------------------------------------
 
 #*change information here for students
-perms <- chooseMatrix(8,XXX)
+perms <- chooseMatrix(8,4)
 A <- matrix(c(0.462, 0.731, 0.571, 0.923, 0.333, 0.750, 0.893, 0.692), nrow=8, ncol=1, byrow=TRUE)
+
+#Define average of treatment and control group. %*% implies matrix multiplication
 treatment_avg <- (1/4)*perms%*%A
-control_avg <- (1/4)*(1-perms)%*%XXX
+control_avg <- (1/4)*(1-perms)%*%A
 test_statistic <- abs(treatment_avg-control_avg)
-rownumber <- apply(apply(perms, 1, 
-                         function(x) (x == c(0, 1, 0, 0, 0, 1, 1, 1))), 
-                   2, sum)
+
+#Apply logic test to perms so that to compare with the treatment/control group of interest
+logic_test <- apply(perms, 1,function(x) (x == c(0, 1, 0, 0, 0, 1, 1, 1)))
+
+#Find the set of treatment/control that fits the empirics. Pick the one that best fits the group of interest.
+rownumber <- apply(logic_test, 2, sum)
 rownumber <- (rownumber == 8)
 observed_test <- test_statistic[rownumber == TRUE]
 
 #*change information here for students
-larger_than_observed <- (test_statistic >= XXX)
+larger_than_observed <- (test_statistic >= observed_test)
 #numbers in which the statistic exceeds the value in the observed date
 sum(larger_than_observed)
 df <- data.frame(perms,control_avg,treatment_avg,test_statistic)
@@ -33,11 +38,16 @@ df <- data.frame(perms,control_avg,treatment_avg,test_statistic)
 #-------------------------------------------------
 simul_stat <- as.vector(NULL)
 schools <- read.csv('teachers_final.csv')
+
+#Set seed to get a reproducible random result
+#?? Need t clarify Pseudorandom number generation
 set.seed(1001)
+
+#?? Still don't understand append()
 for(i in 1:100) {
   print(i)
   schools$rand <- runif(100,min=0,max=1)
-  schools$treatment_rand <- as.numeric(rank(schools$rand)<=49)
+  schools$treatment_rand <- as.numeric(rank(schools$rand)<=49) #pick the first 49 numbers
   schools$control_rand = 1-schools$treatment_rand
   simul_stat <-append(simul_stat,
             sum(schools$treatment_rand*schools$open)/sum(schools$treatment_rand) 
@@ -57,6 +67,7 @@ ate
 control_mean <- sum(schools$control*schools$open)/sum(schools$control)
 treatment_mean <- sum(schools$treatment*schools$open)/sum(schools$treatment)
 
+#Estimate sample variance for control and treatment groups
 s_c <- (1/(sum(schools$control)-1))*sum(((schools$open-control_mean)*schools$control)^2)
 s_t <- (1/(sum(schools$treatment)-1))*sum(((schools$open-treatment_mean)*schools$treatment)^2)
 
